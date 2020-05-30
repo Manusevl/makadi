751ee3a858d77cd5a7ffa22f8c31677f42968031
Sanchez-Marchan Manuel <sanchez-marm@br-automation.co.at>
2020-05-30 18:11:19 +0200

    Implemented functionality to clear the actualOrder

--- client/src/components/Order/OrderOverview/OrderCommands.js
+++ client/src/components/Order/OrderOverview/OrderCommands.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useApolloClient } from "@apollo/react-hooks"
 import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles';
-import {useMutation} from "@apollo/react-hooks";
+import { makeStyles } from '@material-ui/core/styles'
+import {useMutation} from "@apollo/react-hooks"
 import {CREATE_ORDER} from "../../../graphql/mutations"
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 const useStyles = makeStyles({
     confirm: {
@@ -29,29 +30,31 @@ function removeExtraProperties (itemList) {
     })
 }
 
+function clearActualOrder (client) {
+    client.writeData({
+        data: {
+          currentOrder: {
+            __typename: 'Order',
+            _id: uuidv4(),
+            items: []
+          }
+        }
+      })
+}
+
 export default function OrderCommands(props) {
-    console.log(props.itemList)
+    const client = useApolloClient();
     const classesButton = useStyles();
     const [createOrder] = useMutation(CREATE_ORDER, { 
         variables: { items: removeExtraProperties(props.itemList) },
-        update: (cache) => {
-            cache.writeData({
-                data: {
-                  currentOrder: {
-                    __typename: 'Order',
-                    _id: uuidv4(),
-                    items: []
-                  }
-                }
-              })
-          }
+        update: clearActualOrder
     });
     return (
         <div>
             <Button fullWidth={true} className={classesButton.confirm} onClick={createOrder}>
                 Confirm Order
             </Button>
-            <Button fullWidth={true} className={classesButton.cancel}>
+            <Button fullWidth={true} className={classesButton.cancel} onClick={() => clearActualOrder(client)}>
                 Cancel
             </Button>
         </div>
